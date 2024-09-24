57eab526655c9f5eb3b2dfccb6e1fc5d347a4a3e
Prashanth Puranik <puranik@digdeeper.in>
2024-09-24 17:08:28 +0530

    content for Home page

--- applications/workshops-app/src/components/views/Home.vue
+++ applications/workshops-app/src/components/views/Home.vue
@@ -1,6 +1,13 @@
 <template lang="html">
     <div>
-        Home component works!
+        <b-jumbotron header="Workshops App"
+            lead="The Workshops application serves details of (fictitious) technical workshops happening in various cities. Every workshop has a broad topic (eg. JavaScript), and a workshop has many sessions (each session covers a sub-topic, eg. Closures in JavaScript).">
+            <p>
+                You can view a list of workshops, details of every workshop, sessions in a workshop, and also add a new
+                session for a workshop.
+            </p>
+            <b-button tovariant="primary" to="/workshops">Check out nearby workshops</b-button>
+        </b-jumbotron>
     </div>
 </template>
 
